eacbec5fa3ba63c72a6a380b6eeb9b11540655cc
Jose Luis Windows <joseluismodronobw@gmail.com>
2025-03-09 18:20:12 +0100

    Modificación del TaskItem.cs del Backend para evitar warnings al ejecutar dotnet run + Crear un Readme.md general para el backend y el frontend en la raiz del proyecto

--- Backend/TodoApiBackend/Models/TaskItem.cs
+++ Backend/TodoApiBackend/Models/TaskItem.cs
@@ -4,11 +4,11 @@ namespace TodoApiBackend.Models;
 
 public class TaskItem
 {
-    public int Id { get; set; } // Identificador único
-    [Required(ErrorMessage = "El título es obligatorio.")]
-    public string Title { get; set; } // Título de la tarea
+    public int Id { get; set; } // Aquí creo un identificador único para cada tarea.
+    [Required(ErrorMessage = "El título es obligatorio.")] // Creo un mensaje de error en caso de intentar crear un tarea sin título.
+    public string Title { get; set; } = string.Empty; // Título de la tarea. Añado = string.Empty para iniciar la propiedad con un string vacío en lugar de null y así evitar warnings al ejectuar dotnet run.
     [MinLength(10, ErrorMessage = "La descripción debe tener al menos 10 caracteres.")]
-    public string Description { get; set; } // Descripción de la tarea
-    public bool IsCompleted { get; set; } = false; // Estado de la tarea
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow; // Fecha de creación
+    public string Description { get; set; } = string.Empty; // Descripción de la tarea. También añado = string.Empty para evitar warnings.
+    public bool IsCompleted { get; set; } = false; // Estado de la tarea (completada o no completada).
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow; // Fecha de creación de la tarea
 }
--- Backend/TodoApiBackend/obj/Debug/net9.0/TodoApiBackend.AssemblyInfo.cs
+++ Backend/TodoApiBackend/obj/Debug/net9.0/TodoApiBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApiBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+136bd7c227a11c9b4577aed1d9b8e883e04b1e96")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afffd02e0a1be890c6c137870e04a958ff47152f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApiBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApiBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
